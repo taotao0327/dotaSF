fbef810bba83d516490ddc9719798ee2020b8a91
taotao0428 <jigsaw1103@sina.com>
2017-08-09 08:53:22 +0800

    二三血

--- src/com/git/test/ShadowFriend.java
+++ src/com/git/test/ShadowFriend.java
@@ -5,6 +5,8 @@ public class ShadowFriend {
 	public static void main(String[] args) {
 		System.out.println("I am 奈文摩尔");
 		System.out.println("First Blood!");
+		System.out.println("double kill!");
+		System.out.println("threeboule Blood!");
 	}
 
 }
