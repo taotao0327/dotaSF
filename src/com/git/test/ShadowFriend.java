b299c651f3e212470d537fd253fc7efa7648fcec
taotao0327 <673482661@qq.com>
2017-08-09 08:52:19 +0800

    四五血

--- src/com/git/test/ShadowFriend.java
+++ src/com/git/test/ShadowFriend.java
@@ -5,6 +5,8 @@ public class ShadowFriend {
 	public static void main(String[] args) {
 		System.out.println("I am 奈文摩尔");
 		System.out.println("First Blood!");
+		System.out.println("ouchuo kill!");
+		System.out.println("oranpage!");
 	}
 
 }
